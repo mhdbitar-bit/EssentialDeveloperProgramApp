fa4e354d8cbb1ff9a5a7a5225644ac82a81cb2e3
Mohamd Bitar <eng.mhd.bitar@gmail.com>
2022-06-14 07:30:56 +0300

    Make feed loader closure return Paginated<FeedImage>

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -71,7 +71,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
     
-    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
+    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error> {
         let remoteURL = baseURL.appendingPathComponent("/v1/feed")
         
         return httpClient
@@ -79,7 +79,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .tryMap(FeedItemsMapper.map)
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
-        
+            .map {
+                Paginated(items: $0)
+            }
+            .eraseToAnyPublisher()
         // [side-effect]  get from remote server
         // -purefunction- tryMap
         // [side-effect] store in database
