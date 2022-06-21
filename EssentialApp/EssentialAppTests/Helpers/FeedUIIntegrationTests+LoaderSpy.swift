302ef6c4f3b1e5b3c5a901646cba3e87db23cc2d
Mohamd Bitar <eng.mhd.bitar@gmail.com>
2022-06-21 08:11:03 +0300

    Make CoreData FeedImageDataStore implementation sync

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -1,6 +1,7 @@
 //
 //  Copyright © 2019 Essential Developer. All rights reserved.
 //
+
 import Foundation
 import EssentialFeed
 import EssentialFeediOS
@@ -8,19 +9,14 @@ import Combine
 
 extension FeedUIIntegrationTests {
     
-    class LoaderSpy: FeedImageDataLoader {
+    class LoaderSpy {
         
         // MARK: - FeedLoader
         
         private var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
-        private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
         
         var loadFeedCallCount: Int {
             return feedRequests.count
-        }
-        
-        var loadMoreCallCount: Int {
-            return loadMoreRequests.count
         }
         
         func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
@@ -29,37 +25,45 @@ extension FeedUIIntegrationTests {
             return publisher.eraseToAnyPublisher()
         }
         
+        func completeFeedLoadingWithError(at index: Int = 0) {
+            feedRequests[index].send(completion: .failure(anyNSError()))
+        }
+        
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
             feedRequests[index].send(Paginated(items: feed, loadMorePublisher: { [weak self] in
-                let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
-                self?.loadMoreRequests.append(publisher)
-                return publisher.eraseToAnyPublisher()
+                self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
             }))
             feedRequests[index].send(completion: .finished)
         }
         
-        func completeFeedLoadingWithError(at index: Int = 0) {
-            let error = NSError(domain: "an error", code: 0)
-            feedRequests[index].send(completion: .failure(error))
+        // MARK: - LoadMoreFeedLoader
+        
+        private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+        
+        var loadMoreCallCount: Int {
+            return loadMoreRequests.count
+        }
+        
+        func loadMorePublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
+            let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
+            loadMoreRequests.append(publisher)
+            return publisher.eraseToAnyPublisher()
         }
         
         func completeLoadMore(with feed: [FeedImage] = [], lastPage: Bool = false, at index: Int = 0) {
             loadMoreRequests[index].send(Paginated(
-                items: feed,
-                loadMorePublisher: lastPage ? nil : { [weak self] in
-                let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
-                self?.loadMoreRequests.append(publisher)
-                return publisher.eraseToAnyPublisher()
-            }))
+                                            items: feed,
+                                            loadMorePublisher: lastPage ? nil : { [weak self] in
+                                                self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
+                                            }))
         }
         
         func completeLoadMoreWithError(at index: Int = 0) {
-            let error = NSError(domain: "an error", code: 0)
-            loadMoreRequests[index].send(completion: .failure(error))
+            loadMoreRequests[index].send(completion: .failure(anyNSError()))
         }
         
         // MARK: - FeedImageDataLoader
-                
+        
         private var imageRequests = [(url: URL, publisher: PassthroughSubject<Data, Error>)]()
         
         var loadedImageURLs: [URL] {
@@ -85,4 +89,5 @@ extension FeedUIIntegrationTests {
             imageRequests[index].publisher.send(completion: .failure(anyNSError()))
         }
     }
+    
 }
