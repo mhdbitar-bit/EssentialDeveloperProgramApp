201b36ba7fc75611a68f74233cbb182306c2d739
Mohamd Bitar <eng.mhd.bitar@gmail.com>
2022-06-21 07:58:30 +0300

    Rename method to clarify intent

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -7,7 +7,7 @@ import Foundation
 extension CoreDataFeedStore: FeedImageDataStore {
 	
 	public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-		performAsync { context in
+		perform { context in
 			completion(Result {
 				try ManagedFeedImage.first(with: url, in: context)
 					.map { $0.data = data }
@@ -17,10 +17,11 @@ extension CoreDataFeedStore: FeedImageDataStore {
 	}
 	
 	public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-		performAsync { context in
+		perform { context in
 			completion(Result {
 				try ManagedFeedImage.data(with: url, in: context)
 			})
 		}
 	}
+	
 }
